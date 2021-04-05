c92dc3a1ce725fa4af2888ab0f4bf2f6804763ea
Clément Cayrol <clement.cayrol@etu.u-bordeaux.fr>
2021-04-05 19:54:02 +0200

    avancé de get

--- tftp.py
+++ tftp.py
@@ -32,20 +32,28 @@ def runServer(addr, timeout, thread):
         args = frame2.split(b'\x00')                      
         filename = args[0].decode('ascii')                
         mode = args[1].decode('ascii') 
+        s.sendto(data,addr_client)
         if opcode == 1 :                                        #read request
             print("action READ\n")
+            print("adresse du client: ", addr_client[0],"\n")
+            print("port du client : ", addr_client[1])
             try:
                 file_object = open(filename,'rb')
             except Exception as e :
                 print("ERROR:", e)
+            socket_connecté = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            socket_connecté.sendto(bytearray(str(addr_client[1]).encode('ascii')),addr_client)
             requete = bytearray()
             requete.append(0)
             requete.append(3)                    
             for line in file_object :
                 requete += line
-            s.sendto(requete,addr_client)
+            socket_connecté.sendto(requete,addr_client)
+            
         if opcode == 2 :
-            print("action GET\n")
+            print("action GET")
+            print("adresse du client: ", addr_client[0])
+            print("port du client : ", addr_client[1])
             send_ack(addr_client, s)                                       #write request
             file_to_put = open(filename,'rb')
             targetname = open("targetname.txt", 'wb')
@@ -76,8 +84,9 @@ def put(addr, filename, targetname, blksize, timeout):
     mode = bytearray(bytes('octet', 'utf-8'))
     requete += mode
     requete.append(0)
-    s.sendto(requete,('localhost',6969))
-    print("[myclient:",addr[1]," -> myserver:6969] WRQ")
+    host = 'localhost'
+    s.sendto(requete,(host,6969))
+    print("[myclient:",host," -> myserver:6969] WRQ")
     accuse_recep , addr = s.recvfrom(512)
     #if is_ack(accuse_recep) :
     pass
@@ -102,9 +111,21 @@ def get(addr, filename, targetname, blksize, timeout):
     requete += mode
     requete.append(0)
     print("requete émise :",requete)
-    s.sendto(requete,('localhost',6969))
-    data, addr = s.recvfrom(512)
-    print(data)
+    s.sendto(requete,('localhost',addr[1]))
+    data,addr1 = s.recvfrom(512)
+    port, addr2 = s.recvfrom(512)
+    try :
+        data_retour,addr3 =s.recvfrom(512)
+    except :
+        print("error\n")
+    ack = bytearray()
+    ack.append(0)
+    ack.append(4)
+    s.sendto(ack,addr3)
+    print("[myclient:",int(port)," -> myserver :",addr[1],"] RRQ =",data)
+    print("[myserver:",int(addr2[1])," -> myclient : ",int(port),"] DAT1 =",data_retour)
+    print("[myclient:",int(port)," -> myserver:",addr2[1],"] ACK1 =",ack)
+
 
     s.close()
     pass
@@ -116,6 +137,7 @@ def send_ack(addr_client, s) :
     ack.append(0)
     ack.append(4)
     s.sendto(ack,addr_client)
+    
 
 def is_ack(data) :
     frame = data
@@ -124,3 +146,4 @@ def is_ack(data) :
         return True 
     else :
         return False
+
